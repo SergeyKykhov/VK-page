6886300b1fef6527a7432e0cd4382d3758e9b2af
Sergey Kykhov <ghrle@mail.ru>
2022-05-17 22:54:49 +0300

    Create dark gray bacground and adds avatar.

--- VK page/ViewController.swift
+++ VK page/ViewController.swift
@@ -11,9 +11,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+        createImageView()
         // Do any additional setup after loading the view.
     }
 
+    private func setupView() {
+        view.backgroundColor = .tertiaryLabel
+    }
+    private func createImageView() {
+        let imageView = UIImageView(frame: CGRect(x: 15, y: 60, width: 100, height: 100))
+        imageView.image = UIImage(named: "avatar3")
+        imageView.contentMode = .scaleAspectFit
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = imageView.frame.height / 2
+        self.view.addSubview(imageView)
 
+    }
 }
+
 
